3c37d3a19a1645fe7f785c3c2909f6fa70789f29
kayden-s <kayden.spamm@gmail.com>
2025-10-02 21:12:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ st.markdown(
     <style>
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
+    header {visibility: hidden;}
     </style>
     """,
     unsafe_allow_html=True
