c79679226ff6fad1ec4c925f1131365bd73be51c
kayden-s <kayden.spamm@gmail.com>
2025-10-28 01:09:19 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,12 +79,12 @@ def explain_black_scholes():
 
     st.markdown(r"""
     Definitions:
-    - \( S_0 \): Current stock price  
-    - \( K \): Strike price  
-    - \( t \): Time to maturity (in years)  
-    - \( r \): Annual risk-free interest rate  
-    - \( \sigma \): Annualized volatility of returns  
-    - \( N(x) \): Cumulative distribution function of a standard normal variable  
+    - S₀: Current stock price  
+    - K: Strike price  
+    - t: Time to maturity (in years)  
+    - r: Annual risk-free interest rate  
+    - σ: Annualized volatility of returns  
+    - N(x): Cumulative distribution function of a standard normal variable  
 
     ---
     ### Intuition
